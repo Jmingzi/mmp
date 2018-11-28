17c6e0c67117cc4ac41d3fbe5fd3f5bbdd2d3fc3
yangming <472102644@qq.com>
2018-11-28 19:51:22 +0800

    debug: (master)nw 打包的问题

--- src/nw.config.js
+++ src/nw.config.js
@@ -1,70 +1,71 @@
-// if (require) {
-//   const path = './'
-//   const fs = require('fs')
-//
-//   const reloadWatcher = fs.watch(path, () => {
-//     location.reload()
-//     reloadWatcher.close()
-//   })
-//
-//   const win = nw.Window.get()
-//
-//   win.on('close', function(e) {
-//     this.hide()
-//     //(mac上菜单中的退出事件)
-//     if (e === 'quit') {
-//       win.close(true)
-//     }
-//   })
-//
-//   // 初始化托盘图标
-//   var tray = new nw.Tray({
-//     icon: 'icon/icon.icns',
-//     title: 'miss'
-//   })
-//
-//   // 创建菜单
-//   var menu = new nw.Menu()
-//   // 添加子菜单
-//   menu.append(new nw.MenuItem({
-//     type: 'normal',
-//     label: '清除缓存',
-//     click: function(){
-//       nw.App.clearCache()
-//     }
-//   }))
-//   menu.append(new nw.MenuItem({
-//     type: 'normal',
-//     label: '打开控制台',
-//     click: function(){
-//       // nw.App.clearCache()
-//       win.showDevTools()
-//     }
-//   }))
-//   menu.append(new nw.MenuItem({
-//     type: 'normal',
-//     label:'退出',
-//     click: function(){
-//       tray.remove()
-//       tray = null
-//       win.close(true)
-//       process.exit()
-//     }
-//   }))
-//   tray.menu = menu
-//   // 点击托盘图标时激活窗体
-//   tray.on('click',function(){
-//     win.focus()
-//     win.show()
-//   })
-//
-//   nw.App.on('reopen', function(){
-//     win.show()
-//     win.focus()
-//   })
-//
-//   // document.body.addEventListener('contextmenu', function(ev) {
-//     // Prevent showing default context menu
-//     // ev.preventDefault();
-//   // }, false)
-// }
+alert(window.nw)
+if (nw) {
+  const path = './'
+  const fs = require('fs')
+
+  const reloadWatcher = fs.watch(path, () => {
+    location.reload()
+    reloadWatcher.close()
+  })
+
+  const win = nw.Window.get()
+
+  win.on('close', function(e) {
+    this.hide()
+    //(mac上菜单中的退出事件)
+    if (e === 'quit') {
+      win.close(true)
+    }
+  })
+
+  // 初始化托盘图标
+  var tray = new nw.Tray({
+    icon: 'icon/icon.icns',
+    title: 'miss'
+  })
+
+  // 创建菜单
+  var menu = new nw.Menu()
+  // 添加子菜单
+  menu.append(new nw.MenuItem({
+    type: 'normal',
+    label: '清除缓存',
+    click: function(){
+      nw.App.clearCache()
+    }
+  }))
+  menu.append(new nw.MenuItem({
+    type: 'normal',
+    label: '打开控制台',
+    click: function(){
+      // nw.App.clearCache()
+      win.showDevTools()
+    }
+  }))
+  menu.append(new nw.MenuItem({
+    type: 'normal',
+    label:'退出',
+    click: function(){
+      tray.remove()
+      tray = null
+      win.close(true)
+      process.exit()
+    }
+  }))
+  tray.menu = menu
+  // 点击托盘图标时激活窗体
+  tray.on('click',function(){
+    win.focus()
+    win.show()
+  })
+
+  nw.App.on('reopen', function(){
+    win.show()
+    win.focus()
+  })
+
+  // document.body.addEventListener('contextmenu', function(ev) {
+    // Prevent showing default context menu
+    // ev.preventDefault();
+  // }, false)
+}
